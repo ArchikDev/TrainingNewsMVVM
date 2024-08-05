fe979513155ab0bb6238d078139a72bf10046631
a.galeev <a.galeev@malltech-lab.ru>
2024-08-05 20:18:42 +0500

    add OnBoardingScreen

--- app/src/main/java/com/dolgozitbudet/trainingnewsmvvm/MainActivity.kt
+++ app/src/main/java/com/dolgozitbudet/trainingnewsmvvm/MainActivity.kt
@@ -4,23 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import com.dolgozitbudet.trainingnewsmvvm.presentation.onboarding.OnBoardingScreen
 import com.dolgozitbudet.trainingnewsmvvm.ui.theme.TrainingNewsMVVMTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         installSplashScreen()
         setContent {
             TrainingNewsMVVMTheme {
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                    OnBoardingScreen()
+                }
             }
         }
     }
---
+++ app/src/main/java/com/dolgozitbudet/trainingnewsmvvm/presentation/onboarding/Dimens.kt
@@ -0,0 +1,13 @@
+package com.dolgozitbudet.trainingnewsmvvm.presentation.onboarding
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+
+    val MediumPaddding1 = 24.dp
+    val MediumPaddding2 = 30.dp
+
+    val IndicatorSize = 14.dp
+    val PageIndicatorWidth = 52.dp
+
+}
